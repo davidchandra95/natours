6050ac4f2615f5287f75fa0904717b5f565242db
davidchandra95 <david.chandra@tokopedia.com>
2020-06-25 23:56:46 +0700

    chap 6 eps 17

---
+++ controllers/tourControllers.js
@@ -0,0 +1,77 @@
+const fs = require('fs');
+const filepath = `${__dirname}/../dev-data/data/tours-simple.json`;
+
+const tours = JSON.parse(fs.readFileSync(filepath));
+
+exports.getAllTours = (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    requestedAt: req.requestTime,
+    results: tours.length,
+    data: tours,
+  });
+};
+
+exports.getTour = (req, res) => {
+  const tourID = parseInt(req.params.id);
+  const tour = tours.find((el) => el.id === tourID);
+
+  if (!tour) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'invalid id',
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour,
+    },
+  });
+};
+
+exports.createTour = (req, res) => {
+  const newID = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newID }, req.body);
+
+  tours.push(newTour);
+  fs.writeFile(filepath, JSON.stringify(tours), (err) => {
+    res.status(201).json({
+      status: 'success',
+      data: {
+        tour: newTour,
+      },
+    });
+  });
+};
+
+exports.updateTour = (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'invalid id',
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: 'updated tour here',
+    },
+  });
+};
+
+exports.deleteTour = (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'invalid id',
+    });
+  }
+
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
+};
